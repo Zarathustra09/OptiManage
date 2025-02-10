536003a72a23cc7fe42e9b91e844091e88c9a9a7
Zarathustra09 <joshua.pardo30@gmail.com>
2025-02-10 22:56:31 +0800

    February 10 2025 - status config added

---
+++ config/status.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'statuses' => [
+        'to_be_approved' => 'To be Approved',
+        'checked' => 'Checked',
+        'on_progress' => 'On Progress',
+        'finished' => 'Finished',
+        'cancel' => 'Cancel',
+    ],
+];
